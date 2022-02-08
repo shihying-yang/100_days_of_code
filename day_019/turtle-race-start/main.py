e922791f9a17bd0aeac782075d8960a09ebafddf
Shihying Yang <shihying.yang@gmail.com>
2022-02-08 08:14:59 -0500

    day 019: change bet to number, close screen

--- day_019/turtle-race-start/main.py
+++ day_019/turtle-race-start/main.py
@@ -1,7 +1,8 @@
 import random
-from turtle import Turtle, Screen
+import time
+from turtle import Screen, Turtle
 
-STARTING_X = -230
+STARTING_X = -220
 STARTING_Y = -100
 SPACE = 50
 DESTINATION = 220
@@ -13,41 +14,32 @@ is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 
-user_bet = screen.textinput(
-    title="Make your bet", prompt="Which turtle will win the race? Enter a color: "
-).lower()
-
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 all_turtles = []
 
 # create all the turtles and put it in the all_turtle list
-for color in colors:
+for ind, color in enumerate(colors):
     new_turtle = Turtle(shape="turtle")
     new_turtle.color(color)
     new_turtle.penup()
     space_count = colors.index(color)
     x_pos = STARTING_X
     y_pos = STARTING_Y + space_count * SPACE
+    # refactoring: the turtle should print the number for betting
+    new_turtle.goto(x=x_pos - 20, y=y_pos)
+    new_turtle.write(arg=ind + 1, font=("Courier", 16, "bold"))
+    # move the turtle to the start point
     new_turtle.goto(x=x_pos, y=y_pos)
     all_turtles.append(new_turtle)
 
+# refactor: change the input from "color" to "number"
+user_bet = int(
+    screen.numinput(title="Make your bet", prompt="Which turtle will win the race? Enter a number: ")
+)
+
+
 if user_bet:
     is_race_on = True
-    # lazy gaming
-    if user_bet.startswith("r"):
-        user_bet = "red"
-    elif user_bet.startswith("o"):
-        user_bet = "orange"
-    elif user_bet.startswith("y"):
-        user_bet = "yellow"
-    elif user_bet.startswith("g"):
-        user_bet = "green"
-    elif user_bet.startswith("b"):
-        user_bet = "blue"
-    elif user_bet.startswith("p"):
-        user_bet = "purple"
-    else:
-        user_bet = "black"
 
 while is_race_on:
     # real race logic
@@ -56,13 +48,17 @@ while is_race_on:
         the_turtle.forward(random_distance)
         if the_turtle.xcor() > DESTINATION:
             winning_color = the_turtle.pencolor()
-            # end the while loop here
+            winning_turtle = all_turtles.index(the_turtle) + 1
+            # end the while loop if a turtle wins
             is_race_on = False
-            if user_bet == winning_color:
+            if user_bet == winning_turtle:
                 print(f"You've won! The {winning_color} turtle is the winner!")
             else:
                 print(f"You've lost! The {winning_color} turtle is the winner!")
             # prevent another turtle gets to this the final point
             break
 
-screen.exitonclick()
+# refactor: shut off the program after 3 seconds
+time.sleep(3)
+screen.bye()
+
