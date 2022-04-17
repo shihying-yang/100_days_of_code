a8d4d0c25fafb00c56e3b23f8d33ee9750849e89
Shihying Yang <shihying.yang@gmail.com>
2022-04-17 09:26:09 -0400

    day 59: blog final

--- day_059/upgraded-blog/main.py
+++ day_059/upgraded-blog/main.py
@@ -1,10 +1,28 @@
+import requests
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+all_posts = requests.get(blog_url).json()
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/post/<post_id>")
+def get_post(post_id):
+    for post in all_posts:
+        if str(post["id"]) == post_id:
+            return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
