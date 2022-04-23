a13029ef3c4c21de125c7ba7b50e5d76e367219b
Shihying Yang <shihying.yang@gmail.com>
2022-04-23 19:56:02 -0400

    day 61 - bootstrap wtf

--- day_061/flask-secrets/main.py
+++ day_061/flask-secrets/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
-# from wtforms.validators import DataRequired
-
 app = Flask(__name__)
+Bootstrap(app)
 
 app.secret_key = "not_a_secret"
 
@@ -23,11 +23,17 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
     login_form = LoginForm()
-    login_form.validate_on_submit()
-    # if form.validate_on_submit():
-    #     return render_template("success.html")
-    return render_template("login.html", form=login_form)
+    email = login_form.email.data
+    password = login_form.password.data
+    if login_form.validate_on_submit():
+        if email == "admin@email.com" and password == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
+    return render_template("login.html", form=LoginForm())
 
 
 if __name__ == "__main__":
