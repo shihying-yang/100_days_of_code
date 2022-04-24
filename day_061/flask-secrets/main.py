bf76a16aeeddcde4e870d29174e776eef2ca19d1
Shihying Yang <shihying.yang@gmail.com>
2022-04-23 20:18:47 -0400

    day 062 - cafe wifi practice

--- day_061/flask-secrets/main.py
+++ day_061/flask-secrets/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
