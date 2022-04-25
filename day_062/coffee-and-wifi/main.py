ca0da6c42ac4b8c4cb40b206a14e6f9c56153915
Shihying Yang <shihying.yang@gmail.com>
2022-04-25 17:06:09 -0400

    day 62 - finished with full functions

--- day_062/coffee-and-wifi/main.py
+++ day_062/coffee-and-wifi/main.py
@@ -1,32 +1,33 @@
+import csv
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-import csv
+from wtforms import SelectField, StringField, SubmitField
+from wtforms.validators import URL, DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap(app)
 
 
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField("Location", validators=[DataRequired()])
+    cafe = StringField("Cafe name", validators=[DataRequired()])
+    # Exercise:
+    # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
+    # make coffee/wifi/power a select element with choice of 0 to 5.
+    # e.g. You could use emojis ☕️/💪/✘/🔌
+    # make all fields required except submit
+    # use a validator to check that the URL field has a URL entered.
+    # ---------------------------------------------------------------------------
+    location = StringField("Location", validators=[URL()])
     open = StringField("Open")
     close = StringField("Close")
-    coffee = StringField("Coffee", default="☕")
-    wifi = StringField("Wifi", default="📶")
-    power = StringField("Power", default="🔌")
-    submit = SubmitField('Submit')
+    coffee = SelectField("Coffee", choices=["😣", "☕", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], default="☕")
+    wifi = SelectField("Wifi", choices=["❌", "📶", "📶📶", "📶📶📶", "📶📶📶📶", "📶📶📶📶📶"], default="📶")
+    power = SelectField("Power", choices=["❌", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], default="🔌")
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
+    submit = SubmitField("Submit")
 
 
 # all Flask routes below
@@ -35,26 +36,38 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add', methods=["GET", "POST"])
+@app.route("/add", methods=["GET", "POST"])
 def add_cafe():
     form = CafeForm()
-    if form.validate_on_submit():
-        print(f"{form.cafe.data}")
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
-    return render_template('add.html', form=form)
+    if form.validate_on_submit():
+        with open("cafe-data.csv", "a", encoding="utf-8") as f_out:
+            writer = csv.writer(f_out)
+            writer.writerow(
+                [
+                    form.cafe.data,
+                    form.location.data,
+                    form.open.data,
+                    form.close.data,
+                    form.coffee.data,
+                    form.wifi.data,
+                    form.power.data,
+                ]
+            )
+    return render_template("add.html", form=form)
 
 
-@app.route('/cafes')
+@app.route("/cafes")
 def cafes():
-    with open('cafe-data.csv', newline='', encoding="utf-8") as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
+    with open("cafe-data.csv", newline="", encoding="utf-8") as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=",")
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    return render_template("cafes.html", cafes=list_of_rows)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
