691e1f6d6b63191bedd57e5653c516f2d6180691
Shihying Yang <shihying.yang@gmail.com>
2022-05-14 22:14:48 -0400

    day 68: login mechanism

--- day_068/flask-auth/main.py
+++ day_068/flask-auth/main.py
@@ -10,6 +10,17 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+#  Manager Login with Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # return User.get_id(user_id)
+    return User.query.get(int(user_id))
+
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,8 +33,14 @@ class User(UserMixin, db.Model):
 # db.create_all()
 
 
+def hash_password(password):
+    """use generate_password_hash to create password hash to store in the database"""
+    return generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
+
+
 @app.route("/")
 def home():
+    print(hash_password("yang2580"))
     return render_template("index.html")
 
 
@@ -33,10 +50,13 @@ def register():
         new_user = User(
             name=request.form["name"],
             email=request.form["email"],
-            password=request.form["password"],
+            password=hash_password(request.form["password"]),
         )
         db.session.add(new_user)
         db.session.commit()
+        # login and authenticate user
+        login_user(new_user)
+
         return render_template("secrets.html", user=new_user.name)
     return render_template("register.html")
 
@@ -45,22 +65,30 @@ def register():
 def login():
     if request.method == "POST":
         user = db.session.query(User).filter_by(email=request.form["email"]).first()
-        if user.password == request.form["password"]:
+        # if hash_password(request.form["password"]) == user.password:
+        if check_password_hash(user.password, request.form["password"]):
+            login_user(user)
             return redirect(url_for("secrets", user=user.name))
+        else:
+            return "<h1>Wrong Password</h1>"
     return render_template("login.html")
 
 
 @app.route("/secrets")
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    # print(current_user.name)
+    return render_template("secrets.html", user=current_user.name)
 
 
 @app.route("/logout")
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route("/download")
+@login_required
 def download():
     return send_from_directory("static", "files/cheat_sheet.pdf", as_attachment=True)
 
