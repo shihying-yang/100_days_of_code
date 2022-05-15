ddfd21efab5046e8ba93a4d0b10b97f79435ae3c
Shihying Yang <shihying.yang@gmail.com>
2022-05-15 15:23:22 -0400

    day 68: cmplete with login and authentication

--- day_068/flask-auth/main.py
+++ day_068/flask-auth/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, flash, redirect, render_template, request, send_from_directory, url_for
+from flask_login import LoginManager, UserMixin, current_user, login_required, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -17,7 +17,6 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    # return User.get_id(user_id)
     return User.query.get(int(user_id))
 
 
@@ -40,45 +39,54 @@ def hash_password(password):
 
 @app.route("/")
 def home():
-    print(hash_password("yang2580"))
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        user = User.query.filter_by(email=request.form["email"]).first()
+        if user:
+            flash("You've already signed up with that email, login instead.")
+            return redirect(url_for("login"))
+
         new_user = User(
             name=request.form["name"],
             email=request.form["email"],
             password=hash_password(request.form["password"]),
         )
+
         db.session.add(new_user)
         db.session.commit()
         # login and authenticate user
         login_user(new_user)
-
-        return render_template("secrets.html", user=new_user.name)
-    return render_template("register.html")
+        return redirect(url_for("secrets"))
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         user = db.session.query(User).filter_by(email=request.form["email"]).first()
+        if user is None:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for("login"))
         # if hash_password(request.form["password"]) == user.password:
-        if check_password_hash(user.password, request.form["password"]):
+        elif check_password_hash(user.password, request.form["password"]):
             login_user(user)
-            return redirect(url_for("secrets", user=user.name))
+            return redirect(url_for("secrets"))
         else:
-            return "<h1>Wrong Password</h1>"
-    return render_template("login.html")
+            flash("Password incorrect, please try again.")
+            # return "<h1>Wrong Password</h1>"
+            return redirect(url_for("login"))
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/secrets")
 @login_required
 def secrets():
     # print(current_user.name)
-    return render_template("secrets.html", user=current_user.name)
+    return render_template("secrets.html", user=current_user.name, logged_in=current_user.is_authenticated)
 
 
 @app.route("/logout")
