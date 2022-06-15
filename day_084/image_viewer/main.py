fd0da945f7856534030292fef18ac4c99af0714f
Shihying Yang <shihying.yang@gmail.com>
2022-06-15 17:40:30 -0400

    day 84: load the image, but not watermark yet

--- day_084/image_viewer/main.py
+++ day_084/image_viewer/main.py
@@ -1,34 +1,50 @@
 """A Desktop program where you can upload images and add a watermark.
 """
-from tkinter import *
+# from tkinter import *
+import tkinter as tk
 from tkinter import filedialog
-
+from PIL import Image, ImageTk
 
 """Find the correct library to do this
 """
+BASE_IMG_PATH = None
 
-
-def select_image():
+# ---- Load Image from file ---- #
+def load_image():
     """Select an image to show in the viewer"""
-    f = filedialog.askopenfilename(initialdir="/", title="Select an image", filetypes=[("JPEG", "*.jpg"), ("PNG", "*.png")])
-    # f = tk.filedialog.askopenfile(mode="r", filetypes=[("JPEG", "*.jpg"), ("PNG", "*.png")])
-    # ! Stopped here, should return a photoimage object for viewer later
-    print(f)
-
-
-# ----- UI SETUP ----- #
-window = Tk()
-window.title("Image Viewer")
-window.geometry("500x500")
-window.config(padx=20, pady=20, bg="#ffffcc")
-
-canvas = Canvas(width=500, height=500, bg="#ffffcc", highlightthickness=0)
-
-btn = Button(text="Select", highlightthickness=0, command=select_image)
-btn.grid(column=0, row=2, columnspan=2)
+    global BASE_IMG_PATH
+    BASE_IMG_PATH = filedialog.askopenfilename(
+        initialdir=".", title="Select an image", filetypes=[("JPEG", "*.jpg"), ("PNG", "*.png")]
+    )
+    new_img = ImageTk.PhotoImage(file=BASE_IMG_PATH)
+    canvas.itemconfig(canvas_img, image=new_img)
+    canvas.mainloop()
 
 
 # ----- Watermark Mechanism ----- #
+def apply_watermark():
+    # ! stopped here again, use ImageTk to do something tomorrow
+    pass
+
+
+# ----- UI SETUP ----- #
+window = tk.Tk()
+window.title("Image Viewer")
+# window.geometry("500x500")
+window.config(padx=20, pady=20, bg="#ffffcc")
+
+
+btn_load = tk.Button(text="Select Image", highlightthickness=0, command=load_image)
+btn_load.grid(row=0, column=0)
+
+btn_apply = tk.Button(text="Apply Watermark", highlightthickness=0)
+btn_apply.grid(row=0, column=1)
+
+logo_img = tk.PhotoImage(file="no-image.png")
+
+canvas = tk.Canvas(width=500, height=500, bg="#ffffff", highlightthickness=0)
+canvas_img = canvas.create_image(250, 250, image=logo_img)
+canvas.grid(column=0, row=1, columnspan=2, pady=20)
 
 
 window.mainloop()
