70d530185c20875c004e552088aa5bcff5fd7697
Shihying Yang <shihying.yang@gmail.com>
2022-06-18 21:05:59 -0400

    day 84: image viewer complete

--- day_084/image_viewer/main.py
+++ day_084/image_viewer/main.py
@@ -23,8 +23,13 @@ def load_image():
 
 # ----- Watermark Mechanism ----- #
 def apply_watermark():
-    # ! stopped here again, use ImageTk to do something tomorrow
-    pass
+    base_image = Image.open(BASE_IMG_PATH)
+    watermark = Image.open("watermark.png")
+    base_image.paste(watermark, (10, 10))
+    new_image = ImageTk.PhotoImage(base_image)
+    canvas.itemconfig(canvas_img, image=new_image)
+    base_image.save("modified_image.jpg")
+    canvas.mainloop()
 
 
 # ----- UI SETUP ----- #
@@ -37,13 +42,13 @@ window.config(padx=20, pady=20, bg="#ffffcc")
 btn_load = tk.Button(text="Select Image", highlightthickness=0, command=load_image)
 btn_load.grid(row=0, column=0)
 
-btn_apply = tk.Button(text="Apply Watermark", highlightthickness=0)
+btn_apply = tk.Button(text="Apply Watermark", highlightthickness=0, command=apply_watermark)
 btn_apply.grid(row=0, column=1)
 
 logo_img = tk.PhotoImage(file="no-image.png")
 
 canvas = tk.Canvas(width=500, height=500, bg="#ffffff", highlightthickness=0)
-canvas_img = canvas.create_image(250, 250, image=logo_img)
+canvas_img = canvas.create_image(0, 0, image=logo_img, anchor="nw")
 canvas.grid(column=0, row=1, columnspan=2, pady=20)
 
 
